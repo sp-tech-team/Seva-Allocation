0406c8664e51de09e70946272c38147bc74c8180
Adrian Markelov <adrian.markelov@gmail.com>
2024-12-13 12:35:44 +0530

    training data sentence change

--- llama_index_graph_rag/training_data.py
+++ llama_index_graph_rag/training_data.py
@@ -24,9 +24,8 @@ def create_vrf_training_data(vrf_df,
     vrf_generic_df.to_csv(os.path.join(output_dir, "vrf_generic_train_data.csv"), index=False)
     # Make specific training data
     vrf_specific_df = vrf_df[["Request Id"]]
-    vrf_specific_df["summary"] = "A specific job request for the job titled: \"" + vrf_df["Job Title"] + "\" has a request id [" + \
-        vrf_df["Request Id"] + "] with a specific request description: " + vrf_df["Request Name Description"] + \
-        ", and requests the following skills: " + vrf_df["Skills/Keywords"] + ", and " + vrf_df["Add'l Skills"] + \
+    vrf_specific_df["summary"] = "The job titled: \"" + vrf_df["Request Id"] + "\" has a description: " + vrf_df["Request Name Description"] + \
+        ", and expects the following skills: " + vrf_df["Skills/Keywords"] + ", and " + vrf_df["Add'l Skills"] + \
         " and know the languages: " + vrf_df["Languages"] + "."
     vrf_specific_df.to_csv(os.path.join(output_dir, "vrf_specific_train_data.csv"), index=False)
 
